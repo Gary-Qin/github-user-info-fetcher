f65cc02c6687d548e301d30570090170e5dccde5
Gary Qin <garyzhqin@gmail.com>
2023-10-23 19:40:58 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// document.getElementById("myForm").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     let username = document.getElementById("ghname").value;
+
+//     try {
+//         const job = fetch(`https://api.github.com/users/${username}`)
+//         console.log(job);
+//     } catch (error) {
+//         console.log("Error");
+//     }   
+// });
