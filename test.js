42e4ccecdfcb732f776e3ba6a3aeeb25f5821cb1
Gary Qin <garyzhqin@gmail.com>
2023-11-01 16:34:11 -0400

    implementation of remove

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ function MyForm() {
     const [followers, setFollowers] = React.useState("");
     const [saved, setSaved] = React.useState([])
 
-    function handleClick(e) {
+    function handleSubmit(e) {
         e.preventDefault();
     
         const form = e.target;
@@ -19,40 +19,55 @@ function MyForm() {
             })
     }
 
-    function saveUser() {
-        console.log(saved)
+    function handleSave() {
         if(!saved.includes(username) && username != null) {
             setSaved(oldArray => [...oldArray, username]);
-        }        
+        }      
+        console.log(saved)  
+    }
+
+    function handleRemove(value) {
+        setSaved(saved.filter(a => a !== saved[value]));
+        console.log(saved);
     }
 
     function SavedUser({ value }) {
-        return (
-            <div>
-                {value}
-            </div>
-        );
+        if(!saved[value]) { 
+            return (
+                <div>
+                    {saved[value]}
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    {saved[value]}
+                    <button onClick={() => handleRemove(value)}>Remove</button>
+                </div>
+            );
+        }
+        
     }
 
     return (
         <div>
             <div>
-                <form onSubmit={handleClick}>
+                <form onSubmit={handleSubmit}>
                     <label>
                         GitHub Username: <br></br>
                         <input name="githubUsername"/>
                     </label><br></br>
                     <button type="submit">Submit</button>
                 </form>
-                <button onClick={saveUser}>Save</button>
+                <button onClick={handleSave}>Save</button>
                 <p>Name: {username}</p>
                 <p>Followers: {followers}</p>
             </div>
             <div>
                 <h2>Saved:</h2>
-                <SavedUser value={saved[0]}/>
-                <SavedUser value={saved[1]}/>
-                <SavedUser value={saved[2]}/>
+                <SavedUser value={0}/>
+                <SavedUser value={1}/>
+                <SavedUser value={2}/>
             </div>
         </div>
     )
