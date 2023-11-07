c6d12cc989b10ed14942817219e712f3f2358c11
Gary-Qin <52765139+Gary-Qin@users.noreply.github.com>
2023-11-06 22:42:14 -0500

    Implemented objects in saved array

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const app = document.getElementById('app');
 
 function MyForm() {
-    const [username, setUsername] = React.useState("");
-    const [followers, setFollowers] = React.useState("");
+    const [curName, setCurName] = React.useState("");
+    const [curLogin, setCurLogin] = React.useState("");
+    const [curFollowers, setCurFollowers] = React.useState("");
+    const [curAvatar, setCurAvatar] = React.useState("");
     const [saved, setSaved] = React.useState([])
 
     function handleSubmit(e) {
@@ -14,38 +16,50 @@ function MyForm() {
         fetch(`https://api.github.com/users/${formJson.githubUsername}`)
             .then(res => res.json())
             .then(data => {
-                setUsername(data.name)
-                setFollowers(data.followers)
+                setCurName(data.name)
+                setCurLogin(data.login)
+                setCurFollowers(data.followers)
+                setCurAvatar(data.avatar_url);
             })
     }
 
     function handleSave() {
-        if(!saved.includes(username) && username != null && saved.length < 3) {
-            setSaved(oldArray => [...oldArray, username]);
-        }      
-    }
-
-    function handleRemove(value) {
-        setSaved(saved.filter(a => a !== saved[value]));
+        // if(!saved.includes(username) && username != null && saved.length < 3) {
+        //     setSaved(oldArray => [...oldArray, {name: username,}]);
+        // }
+        let exists = 0;
+        for(const i of saved) {
+            if(i.name == curName) {
+                exists = 1;
+            }
+        }
+        if(!exists && saved.length < 3) {
+            setSaved(oldArray => [...oldArray, {name: curName, login: curLogin, followers: curFollowers, avatar: curAvatar}]);
+        }
         console.log(saved);
     }
 
-    function SavedUser({ value }) {
-        if(!saved[value]) { 
-            return (
-                <div>
-                    {saved[value]}
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    {saved[value]}
-                    <button onClick={() => handleRemove(value)}>Remove</button>
-                </div>
-            );
-        }
+    function handleRemove(value) {
+        setSaved(saved.filter(a => a.name !== saved[value].name));
+        console.log(saved);
     }
+
+    // function SavedUser({ value }) {
+    //     if(!saved[value]) { 
+    //         return (
+    //             <div>
+    //                 {saved[value].name}
+    //             </div>
+    //         );
+    //     } else {
+    //         return (
+    //             <div>
+    //                 {saved[value].name}
+    //                 <button onClick={() => handleRemove(value)}>Remove</button>
+    //             </div>
+    //         );
+    //     }
+    // }
 
     return (
         <div>
@@ -58,15 +72,15 @@ function MyForm() {
                     <button type="submit">Submit</button>
                 </form>
                 <button onClick={handleSave}>Save</button>
-                <p>Name: {username}</p>
-                <p>Followers: {followers}</p>
+                <p>Name: {curName}</p>
+                <p>Followers: {curFollowers}</p>
             </div>
-            <div>
+            {/* <div>
                 <h2>Saved:</h2>
                 <SavedUser value={0}/>
                 <SavedUser value={1}/>
                 <SavedUser value={2}/>
-            </div>
+            </div> */}
         </div>
     )
 }
